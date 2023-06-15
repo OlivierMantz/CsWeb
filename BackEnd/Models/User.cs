bf35c894bd6267fd6131df4d1b32e243889d4c9b
OlivierMantz <OlivierMantz>
2023-06-15 23:03:43 +0200

    Updated User model

--- BackEnd/Models/User.cs
+++ BackEnd/Models/User.cs
@@ -1,13 +1,17 @@
-﻿namespace BackEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models
 {
     public class User
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-        public bool IsBanned { get; set; }
+        [Key] public long Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] [EmailAddress] public string Email { get; set; }
 
+        [Required] [MinLength(6)] public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public bool IsBanned { get; set; }
     }
 }
